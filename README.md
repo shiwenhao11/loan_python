62064f71455f9ae3c977992895d4d7afa0d41f0d
陆岳 <luyue@nonobank.com>
2017-07-22 10:27:20 +0800

    提交初始化的框架nose

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # loan_python
 python testing
+
+
+nose-html-reporting报告的本地化和显示优化  
+命令：
+nosetests --with-html --html-report=output/test_report.html --html-report-template=/Library/Python/2.7/site-packages/nose_html_reporting/templates/report2.jinja2
+
+依赖包安装
+执行以下命令：
+pip install -r requirements.txt
+注意：requirements.txt 不要轻易改动，改动请通知大家
---
+++ test_hello_demo.py
@@ -0,0 +1,31 @@
+#coding=utf-8
+import random
+# class Create_Mobile_Num:
+#     def create_mobile_num(self,nu):
+#         self.num=num
+#
+#
+# mobile_num = random.choice(['139','138','185','136','158','159'])+"".join(random.choice("0123456789") for i in range(8))
+#
+# print mobile_num
+
+#模拟get请求
+# import urllib,urllib2
+# url='https://m.stb.nonobank.com/feserver/mzjk/promotion/home'
+# textmod ={'user':'admin','password':'admin'}
+# textmod = urllib.urlencode(textmod)
+# print(textmod)
+# #输出内容:password=admin&user=admin
+# req = urllib2.Request(url)
+# res = urllib2.urlopen(req)
+# res = res.read()
+# print(res)
+#输出内容:登录成功
+
+
+def test_numbers_3_4():
+
+    assert 1 == 1
+
+
+
